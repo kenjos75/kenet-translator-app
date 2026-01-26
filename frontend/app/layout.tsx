0cea28a9425ead8e76d6efd67bf9a29a509850d7
Kenneth <kenjos75@gmail.com>
2026-01-26 16:53:33 +0800

    feat: added query provider in order to apiRequest wrapper.

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { queryClient } from "@/app/lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <QueryClientProvider client={queryClient}>
         {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
